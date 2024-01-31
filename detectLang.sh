7e3597d0cfb98f9d13e781baf3ae1cc2370ae5fb
Elango Senthilnathan <144163174+elangosenthilnathan@users.noreply.github.com>
2024-01-31 09:39:20 -0800

    Update detectLang.sh

--- detectLang.sh
+++ detectLang.sh
@@ -24,7 +24,7 @@ export SHIFTLEFT_ACCESS_TOKEN=$token
 
 HIDDEN_DIR=$(ls -a $path | grep '.git')
 
-if [[ -v ${HIDDEN_DIR} ]]; then
+if [ -v ${HIDDEN_DIR} ]; then
   git init 2> /dev/null > /dev/null
   git config user.name "githubtest"
   git add --all 2> /dev/null > /dev/null
