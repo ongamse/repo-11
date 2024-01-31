15441d1c94a98509411970072082ccebafc9d2ea
Elango Senthilnathan <144163174+elangosenthilnathan@users.noreply.github.com>
2024-01-31 09:51:48 -0800

    Update detectLang.sh

--- detectLang.sh
+++ detectLang.sh
@@ -26,7 +26,6 @@ HIDDEN_DIR=$(ls -a $path | grep '.git')
 
 if [ -z "${HIDDEN_DIR}" ]; then
   echo "${HIDDEN_DIR} is empty"
-else
   git init 2> /dev/null > /dev/null
   git config user.name "githubtest"
   git add --all 2> /dev/null > /dev/null
